a43afb01790ff74b313f86d7051b3f3ae00d9929
Nataliya Stepanova <gnykka@gmail.com>
2020-05-31 01:43:54 +0300

    Set fixed svg position while scrolling through text

--- src/scripts/intro.js
+++ src/scripts/intro.js
@@ -2,15 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const body = document.querySelector('body');
   const section = document.querySelector('section.intro');
 
-  const text = section.querySelector('div');
+  const text = section.querySelector('.content');
   const svg = section.querySelector('svg');
 
   const height = body.clientHeight;
-  const svgPadding = parseFloat(window.getComputedStyle(svg, null).getPropertyValue('top'));
-  const svgTop = section.getBoundingClientRect().top;
+  const { top, bottom } = section.getBoundingClientRect();
+  const { scrollY } = window;
+
+  const svgPadding = parseFloat(window.getComputedStyle(section, null).getPropertyValue('padding-top'));
+  const svgTop = top + scrollY;
+  const svgBottom = bottom + scrollY - height;
 
   window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
 
+    svg.classList.toggle('fixed', scroll >= svgTop && scroll < svgBottom);
+    svg.classList.toggle('bottom', scroll >= svgBottom);
   });
 
   svg.style.height = `${height - svgPadding * 2}px`;
