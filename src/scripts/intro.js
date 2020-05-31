696fc846496b94b8e3735691e08cd0feb6f75d7f
Nataliya Stepanova <gnykka@gmail.com>
2020-05-31 03:09:09 +0300

    Implement animation of svg images on scroll

--- src/scripts/intro.js
+++ src/scripts/intro.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const text = section.querySelector('.content');
   const svg = section.querySelector('svg');
 
+  const svgBackground = svg.querySelector('g.background');
+  const svgGroups = svg.querySelectorAll('g.group');
+
   const height = body.clientHeight;
   const { top, bottom } = section.getBoundingClientRect();
   const { scrollY } = window;
@@ -13,12 +16,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const svgTop = top + scrollY;
   const svgBottom = bottom + scrollY - height;
 
+  const svgHeight = svgBottom - svgTop;
+  const animationDistance = Math.round(svgHeight / svgGroups.length);
+
+  const hideSvg = () => {
+    svgBackground.style.opacity = 0;
+    svgGroups.forEach((gr, i) => {
+      gr.style.opacity = 0;
+    });
+  };
+
   window.addEventListener('scroll', () => {
     const scroll = window.scrollY;
+    const isInside = scroll >= svgTop && scroll < svgBottom;
 
-    svg.classList.toggle('fixed', scroll >= svgTop && scroll < svgBottom);
+    svg.classList.toggle('fixed', isInside);
     svg.classList.toggle('bottom', scroll >= svgBottom);
+
+    if (isInside) {
+      const value = (scroll - svgTop) / animationDistance;
+      const index = Math.floor(value);
+      const opacity = value - index;
+
+      svgBackground.style.opacity = 1;
+
+      svgGroups.forEach((gr, i) => {
+        if (i === index) {
+          gr.style.opacity = opacity;
+        } else if (index > 0 && i === index - 1) {
+          gr.style.opacity = 1;
+        } else {
+          gr.style.opacity = 0;
+        }
+      });
+    } else if (scroll < svgTop) {
+      hideSvg();
+    }
   });
 
   svg.style.height = `${height - svgPadding * 2}px`;
+  hideSvg();
 });
