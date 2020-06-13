240912c0e24dd98386bcbe8d1fe60b4b433ced37
Nataliya Stepanova <gnykka@gmail.com>
2020-06-13 23:24:30 +0300

    Add images and fix styles of coin

--- src/scripts/coin.js
+++ src/scripts/coin.js
@@ -3,15 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const section = document.querySelector('section.coin');
   const button = section.querySelector('button');
-  const resultEl = section.querySelector('.result');
-  const countEl = section.querySelector('.count span');
   const eagleEl = section.querySelector('.eagle span');
   const tailsEl = section.querySelector('.tails span');
   const coin = section.querySelector('.animation');
 
   const imgs = coin.querySelectorAll('img');
 
-  let count = 0;
   let eagle = 0;
   let tails = 0;
 
@@ -26,14 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
       const res = random();
 
-      count += 1;
       if (res === 0) eagle += 1;
       else tails +=1;
 
-      countEl.innerText = count;
-      eagleEl.innerText = `${Math.round(eagle * 100 / count)}%`;
-      tailsEl.innerText = `${Math.round(tails * 100 / count)}%`;
-      resultEl.innerText = `Выпал${res === 0 ? ' орёл' : 'а решка'}`;
+      eagleEl.innerText = eagle;
+      tailsEl.innerText = tails;
 
       button.classList.toggle('disabled', false);
       coin.classList.remove('toss');
