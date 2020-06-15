d10ffe67e8e7b8a419ea0899a979e19202758cf4
Nataliya Stepanova <gnykka@gmail.com>
2020-06-15 19:46:56 +0300

    Fix animation

--- src/scripts/regularity.js
+++ src/scripts/regularity.js
@@ -18,20 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
     svg.classList.toggle('bottom', bottom < height);
 
     const phase1 = texts[1].getBoundingClientRect().top - height * 0.25 <= 0;
-    const phase2 = texts[1].getBoundingClientRect().top + height * 0.25 <= 0;
-
-    const phase3 = texts[2].getBoundingClientRect().top - height * 0.1 <= 0;
-    const phase4 = texts[2].getBoundingClientRect().top + height * 0.3 <= 0;
-    const phase5 = texts[2].getBoundingClientRect().top + height * 0.6 <= 0;
-    const phase6 = texts[2].getBoundingClientRect().top + height * 0.9 <= 0;
+    const phase2 = texts[2].getBoundingClientRect().top - height * 0.1 <= 0;
+    const phase3 = texts[3].getBoundingClientRect().top - height * 0.25 <= 0;
+    const phase4 = texts[4].getBoundingClientRect().top - height * 0.25 <= 0;
+    const phase5 = texts[5].getBoundingClientRect().top - height * 0.25 <= 0;
 
     svg.classList.toggle('phase-0', !phase1);
     svg.classList.toggle('phase-1', phase1 && !phase2);
     svg.classList.toggle('phase-2', phase2 && !phase3);
     svg.classList.toggle('phase-3', phase3 && !phase4);
     svg.classList.toggle('phase-4', phase4 && !phase5);
-    svg.classList.toggle('phase-5', phase5 && !phase6);
-    svg.classList.toggle('phase-6', phase6 || bottom < height);
+    svg.classList.toggle('phase-5', phase5 || bottom < height);
   }
 
   window.addEventListener('scroll', onScroll);
@@ -40,5 +37,5 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // вначале — маска — первый абзац
-// phase1 и phase2 (меняющаяся фигура) - второй абзац
-// третий абзац: phase3 (голова), phase4 (змеи), phase5 (обезьяны), phase6 (рука)
+// phase1 (фигура) - второй абзац
+// третий-шестой абзацы: phase2 (голова), phase3 (змеи), phase4 (обезьяны), phase5 (рука)
