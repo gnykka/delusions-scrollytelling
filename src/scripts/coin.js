eef4e3e9769d0e0cc6481f4a6f50555b4b14eea3
Nataliya Stepanova <gnykka@gmail.com>
2020-06-16 20:56:03 +0300

    Show percents instead of counts in coin section

--- src/scripts/coin.js
+++ src/scripts/coin.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const button = section.querySelector('button');
   const eagleEl = section.querySelector('.eagle span');
   const tailsEl = section.querySelector('.tails span');
+  const countEl = section.querySelector('.count');
   const coin = section.querySelector('.animation');
 
   const imgs = coin.querySelectorAll('img');
@@ -26,8 +27,14 @@ document.addEventListener('DOMContentLoaded', () => {
       if (res === 0) eagle += 1;
       else tails +=1;
 
-      eagleEl.innerText = eagle;
-      tailsEl.innerText = tails;
+      const count = eagle + tails;
+
+      eagleEl.innerText = Math.round(eagle / count * 100);
+      tailsEl.innerText = Math.round(tails / count * 100);
+
+      if (count % 10 === 1 && count % 100 !== 11) countEl.innerText = `${count} бросок`;
+      else if (count % 10 > 1 && count % 10 < 5 && !(count % 100 > 11 && count % 100 < 15)) countEl.innerText = `${count} броска`;
+      else countEl.innerText = `${count} бросков`;
 
       button.classList.toggle('disabled', false);
       coin.classList.remove('toss');
